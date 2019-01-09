064f641be39bf734975965b4e25f4e18824b29b6
Alex <alexb@updg.net>
2019-01-09 00:03:41 -0800

    Bacause of CGO switching to in-docker build. Thanks to xgo for skeleton and docker images.

--- build.sh
+++ build.sh
@@ -2,15 +2,19 @@
 cd $(dirname "${BASH_SOURCE[0]}")
 OD="$(pwd)"
 # Pushes application version into the build information.
-ACRON_VERSION=1.0.0
+ACRON_VERSION=1.1.0
 
 build(){
 	echo Packaging $1 Build
 	bdir=acron-${ACRON_VERSION}-$2-$3
 	rm -rf builds/$bdir && mkdir -p builds/$bdir
-	GOOS=$2 GOARCH=$3 ./build.sh
 
-	mv acron builds/$bdir
+	cd build
+	docker-compose run builder ./build/build_platform.sh $2/$3
+	cd ..
+
+    mv build/acron-$2-$3 build/acron
+	mv build/acron builds/$bdir
 
 	cp README.md builds/$bdir
 	cp LICENSE builds/$bdir
@@ -30,7 +34,6 @@ if [ "$1" == "all" ]; then
 	rm -rf builds/
 	build "Mac" "darwin" "amd64"
 	build "Linux" "linux" "amd64"
-	build "FreeBSD" "freebsd" "amd64"
 	exit
 fi
 
